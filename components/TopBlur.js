11bfb7f6e42bb0fed0c969c86b11238f9e8f9fd1
facuperezm <facundoperezmontalvo@gmail.com>
2022-11-11 14:26:40 -0300

    redeploy

--- components/TopBlur.js
+++ components/TopBlur.js
@@ -6,7 +6,7 @@ const TopBlur = styled.div`
   height: 15%;
   top: 0;
   left: 0;
-  background: linear-gradient(180deg, #1f2028, rgba(32, 32, 35, 0.01) 90%);
+  background: linear-gradient(180deg, #1c1c1c, rgba(32, 32, 35, 0.01) 90%);
   mask-image: linear-gradient(#1f2028, transparent);
   backdrop-filter: blur(4px);
   zindex: 5;
